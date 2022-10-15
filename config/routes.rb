284ecc23e69e580083a48c76edb16816a2f2b5b0
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-10-15 10:23:00 +0200

    new rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+  # delete "restaurants/:id", to: "restaurants#destroy"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:create, :index, :destroy, :edit]
 end
